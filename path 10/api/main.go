fa8cfdb48f5a25131543444ddb43adaea3137316
514K <alekseysafronov0@gmail.com>
2023-03-13 00:03:16 +0300

    create mod

--- path 10/api/main.go
+++ path 10/api/main.go
@@ -1,5 +1,14 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
 func main() {
 	router := gin.Default()
+
+	router.POST("/sotrudnik")
+	router.GET("/sotrudnik/:id")
+	router.PUT("/sotrudnik/:id")
+	router.DELETE("/sotrudnik/:id")
+
+	router.Run()
 }
